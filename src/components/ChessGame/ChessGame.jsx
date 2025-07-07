566e14d1299b39c71d149f8023bb5898c3cd8c96
ValerieTsan <valeriejean.tsan@gmail.com>
2025-07-07 14:09:50 -0400

    Fix scafolding project structure

--- src/components/ChessGame/ChessGame.jsx
+++ src/components/ChessGame/ChessGame.jsx
@@ -1,10 +1,11 @@
 import { Chess } from "chess.js";
 import { useState } from "react";
-
 import "../ChessGame/ChessGame.css";
 
 const ChessGame = () => {
   const [game, setGame] = useState(new Chess());
+  const [moveLog, setMoveLog] = useState([]);
+
   return <div></div>;
 };
 
